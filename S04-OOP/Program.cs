3e1c191dc372b63676e774b50553820aacac7ca0
msavarian <sav68.net@gmail.com>
2023-10-10 21:48:26 +0330

    finish s04

--- S04-OOP/Program.cs
+++ S04-OOP/Program.cs
@@ -1,1 +1,71 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using S04_OOP.SampleClasses;
+
+
+
+/*
+ * Letter Case
+ *
+ * camelCase    ===== iAmMahmoud,   tempAgeOfCompany
+ * PascalCase   ===== IAmMahmoud,   TempAgeOfCompany
+ * kebab-case   ===== i-am-mahmoud,   temp-age-of-company
+ * Snake_Case   ===== I_Am_Mahmoud,   Temp_Age_Of_Company
+ *
+ * Naming Rule
+ * Naming Convention
+ *
+ */
+
+int a = new int();
+a = 5;
+
+int age = 5;
+
+
+string name1 = new string("hasan");
+string name = "hasan";
+
+
+var pride = new Car();
+pride.color = "red";
+pride.engineNumber = "515215150";
+pride.vin = "2510518515";
+
+
+Console.WriteLine("Car Name " + Car.name);
+Car.name = "hasan";
+Console.WriteLine("Car Name " + Car.name);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Vin Number " + pride.vin);
+pride.vin = "33333333333";
+Console.WriteLine("Vin Number " + pride.vin);
+
+/*
+ * Types
+ *  Primitive   Types (int, string, bool, ...)
+ *  Pre-Define  Types (object, ...)
+ *  User-Define Types (car, Student, University, ...)
+ * 
+ * Types (Class, Struct, Interface, Record, ...)
+ * 
+ * Class Members
+ *      Method, Constructor, Fields, Properties, ...
+ *
+ * Static Members
+ *
+ * Access Modifiers
+ *
+ *
+ */
+
+
+/*
+ * OOP
+ *
+ *  Inheritance
+ *  Polymophysm
+ *  Encapsoulation
+ *  Abstraction
+ */
---
+++ S04-OOP/SampleClasses/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S04_OOP.SampleClasses
+{
+    public class Car
+    {
+        public static string name = "Pride";
+        
+        public string model = "";
+        public string color = "";
+        public string vin = "";
+        public string engineNumber = "";
+        
+        private int _speed = 0;
+
+
+        public void ChangeColor(string newColor)
+        {
+            color=newColor;
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void IncreaseSpeed(int kph)
+        {
+            _speed = kph;
+        }
+
+
+    }
+}
