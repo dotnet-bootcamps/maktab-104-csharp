a83b0f564933d7b09ec13cdb76f058d476dc4042
msavarian <sav68.net@gmail.com>
2024-02-20 21:00:23 +0330

    1st ef session

---
+++ S34-ConsoleApp-EfCore-Intro/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using S34_ConsoleApp_EfCore_Intro.Models;
+
+namespace S34_ConsoleApp_EfCore_Intro
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(LocalDb)\\MSSQLLocalDB; Initial Catalog=MaktabSharif-104; Integrated Security=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
--- S34-ConsoleApp-EfCore-Intro/Program.cs
+++ S34-ConsoleApp-EfCore-Intro/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
+using S34_ConsoleApp_EfCore_Intro;
+using S34_ConsoleApp_EfCore_Intro.Models;
+
 Console.WriteLine("Hello, World!");
+
+var db = new AppDbContext();
+
+
+// *** insert
+
+//for (int i = 0; i < 10; i++)
+//{
+//    var newProduct = new Product();
+//    newProduct.Id = i + 100;
+//    newProduct.Title = $"Product-{i + 100}";
+
+//    db.Products.Add(newProduct);
+//}
+//db.SaveChanges();
+
+
+// *** update
+var q = db.Products.Where(w=>w.Title=="Livan").FirstOrDefault();
+if (q != null)
+{
+    q.Title = "glass";
+    db.SaveChanges();
+}
+
+
+// *** delete
+//var result = db.Products.Find(104);
+var result = db.Products.FirstOrDefault(w=> w.Id == 104);
+if (result != null)
+{
+    db.Products.Remove(result);
+    db.SaveChanges();
+}
+
+
+
+
+//var sqlQuery = db.Products.ToQueryString();
+//var products = sqlQuery.ToList();
+var products = db.Products.AsNoTracking().ToList();
+
+foreach (var item in products)
+{
+    Console.WriteLine($"Id={item.Id} Title={item.Title}");
+}
