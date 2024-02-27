5e1b024910df80c05687bff0fb43e90e8d23b5c4
msavarian <sav68.net@gmail.com>
2024-02-27 19:39:58 +0330

    Add Entities

--- S34-ConsoleApp-EfCore-Intro/Models/Product.cs
+++ S34-ConsoleApp-EfCore-Intro/Models/Product.cs
@@ -10,6 +10,8 @@ namespace S34_ConsoleApp_EfCore_Intro.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public int Price { get; set; }
+
 
     }
 }
---
+++ S36-ConsoleApp-EfCore-DbDesign/Entities/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S36_ConsoleApp_EfCore_DbDesign.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //public int LikeId { get; set; }
+        //public List<Like> Likes { get; set; }
+    }
+
+    public class Like
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        //public int LikeId { get; set; }
+        //public List<Like> Likes { get; set; }
+    }
+
+
+
+}
+
+
+
+
